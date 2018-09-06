b06940b2a333cc64df3240686d881d35f97c6979
dingzhengfuaa <dingzhengfuaa@gmail.com>
2018-09-06 09:55:43 -0700

    started aging people

--- ios_stack/iOS_intermediate/Beast List/Beast List/ViewController.swift
+++ ios_stack/iOS_intermediate/Beast List/Beast List/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+// Create this array at the top of your class as a property
+var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        let task: String = taskTextField.text!
+        tasks.append(task)
+        tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    // MAKE SURE THESE ARE WITHIN UITableViewDataSource EXTENSION!copy
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return tasks.count
+    }
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the UITableViewCell and create/populate it with data then return it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        // set text label to the model that is corresponding to the row in array
+        cell.textLabel?.text = tasks[indexPath.row]
+        // return cell so that Table View knows what to render in each rowcopy
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
 }
 
